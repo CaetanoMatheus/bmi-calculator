5cd1cb30120d378a9b4b97d0f70aa6796749f424
Matheus Caetano <caetanomatheus876@gmail.com>
2020-11-21 11:05:21 -0300

    Final Template

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Calculadora de IMC'),
         centerTitle: true,
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.green,
         actions: [
           IconButton(
             icon: Icon(Icons.refresh),
@@ -32,29 +32,51 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.person_outline, size: 120, color: Colors.pink),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(color: Colors.pink),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_outline, size: 120, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Peso (kg)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 25),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Altura (cm)',
-              labelStyle: TextStyle(color: Colors.pink),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Altura (cm)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 25),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Container(
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Calcular',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Text(
+              'Info',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25),
+            ),
+          ],
+        ),
       ),
     );
   }
